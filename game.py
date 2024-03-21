50de4c5f0749e97a6363243b0a60f74292fc3995
Priit <priit.pajumae@voco.ee>
2024-03-21 22:18:20 +0200

    Add player and its game logic

--- game.py
+++ game.py
@@ -2,22 +2,22 @@
 
 import pygame
 from settings import Settings
-
-pygame.init()
-gm_settings = Settings()
-
-screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
-pygame.display.set_caption(gm_settings.caption)
+from player import Player
+import game_functions as gf
 
 
-running = True
-while running:
-    screen.fill(gm_settings.bg_color)    
-    for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                    running = False
+def run_game():
+    pygame.init()
+    gm_settings = Settings()
+    
+    screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
+    pygame.display.set_caption(gm_settings.caption)
 
-    pygame.display.flip()
-
-pygame.quit() 
-
+    player = Player(screen)
+    
+    while True:
+        gf.check_events(player)
+        player.update()
+        gf.update_screen(gm_settings, screen, player)
+    
+run_game()
