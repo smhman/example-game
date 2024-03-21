5ff08b566ff8d28e6bc14eabb4181efeb026f67b
Priit <priit.pajumae@voco.ee>
2024-03-21 23:28:29 +0200

    Add bubble and and player collision

--- game.py
+++ game.py
@@ -11,6 +11,8 @@ def run_game():
     screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
     pygame.display.set_caption(gm_settings.caption)
     
+    clock = pygame.time.Clock()
+    
     player = Player(screen)
     
     bubbles = pygame.sprite.Group()
@@ -18,7 +20,8 @@ def run_game():
     while True:
         gf.check_events(gm_settings, screen, player, bubbles)
         player.update()
+        gf.update_bubbles(player, bubbles)
         bubbles.update()
-        gf.update_screen(gm_settings, screen, player, bubbles)
+        gf.update_screen(gm_settings, screen, player, bubbles, clock)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -35,11 +35,17 @@ def check_events(game_settings, screen, player, bubbles):
 def create_bubble(game_settings, screen, bubbles):
     new_bubble = Bubble(screen, game_settings)
     bubbles.add(new_bubble)
+
+def update_bubbles(player, bubbles):
+    hitted_bubble = pygame.sprite.spritecollideany(player, bubbles)
+    if hitted_bubble != None:
+        hitted_bubble.kill()
             
             
-def update_screen(game_settings, screen, player, bubbles):
+def update_screen(game_settings, screen, player, bubbles, clock):
     screen.fill(game_settings.bg_color)
     player.blit_me()
     for bubble in bubbles:
         bubble.blit_me()
+    clock.tick(30)
     pygame.display.flip()
